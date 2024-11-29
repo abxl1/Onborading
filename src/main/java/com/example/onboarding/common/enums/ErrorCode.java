f5a3190d0422dbe35ce081a389e895c3edac450d
abxl1 <etho_818@naver.com>
2024-11-29 10:00:00 +0900

    [FIX] add ENUM

--- src/main/java/com/example/onboarding/common/enums/ErrorCode.java
+++ src/main/java/com/example/onboarding/common/enums/ErrorCode.java
@@ -6,7 +6,9 @@ import org.springframework.http.HttpStatus;
 @Getter
 public enum ErrorCode {
 
-    USER_ROLE_NOT_FOUND(HttpStatus.NOT_FOUND, "대상을 찾지 못했습니다."),
+    USER_ROLE_NOT_FOUND(HttpStatus.NOT_FOUND, "사용자를 찾지 못했습니다."),
+    TOKEN_NOT_FOUND(HttpStatus.NOT_FOUND, "토큰을 찾을 없습니다."),
+
     NOT_FOUND(HttpStatus.NOT_FOUND, "대상을 찾지 못했습니다.");
 
     private final HttpStatus status;
