ca77928579671533f7ce623d3efac9b7db404444
abxl1 <nyong@nayong-ui-MacBookAir.local>
2024-12-09 20:49:18 +0900

    [FEAT] add JWT class

---
+++ src/main/java/com/example/onboarding/config/JwtSecurityFilter.java
@@ -0,0 +1,110 @@
+package com.example.onboarding.config;
+
+
+import com.example.onboarding.common.dto.AuthUser;
+import com.example.onboarding.domain.user.enums.UserRole;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.MalformedJwtException;
+import io.jsonwebtoken.UnsupportedJwtException;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class JwtSecurityFilter extends OncePerRequestFilter {
+
+    /*
+    OncePerRequestFilter 을 상속받아 매 요청마다 한 번만 필터링하도록 처리
+    HTTP 요청의 헤더에서 JWT 를 추출하고 이를 검증하여 인증 정보를 설정
+    JWT 에 대한 유효성 검사를 수행하고 발생하는 다양한 예외에 대해 적절한 HTTP 응답을 반환
+    최종적으로 필터 체인을 계속 진행하여 다른 필터나 핸들러로 요청을 전달
+     */
+
+    private final JwtUtil jwtUtil;
+
+    /**
+     * OncePerRequestFilter 추상 클래스의 추상 메서드 구현체
+     * HTTP 요청을 필터링하고 필요한 인증 절차를 수행
+     * SecurityContextHolder 에 인증 정보를 설정
+     *
+     * @param httpRequest  Http 요청 객체
+     * @param httpResponse Http 응답 객체
+     * @param chain        다음 필터 또는 요청 처리기로 요청 전달
+     * @throws ServletException Http 요청 처리 중 발생할 수 있는 예외
+     * @throws IOException      입출력 처리 중 발생할 수 있는 예외
+     */
+    @Override
+    protected void doFilterInternal(
+            HttpServletRequest httpRequest,
+            @NonNull HttpServletResponse httpResponse,
+            @NonNull FilterChain chain
+    ) throws ServletException, IOException {
+
+        // Authorization 헤더에서 JWT 를 추출
+        String authorizationHeader = httpRequest.getHeader("Authorization");
+
+        // JWT 가 "Bearer "로 시작하는지 판별
+        if (authorizationHeader != null && authorizationHeader.startsWith("Bearer ")) {
+
+            // JWT 문자열에서 실제 토큰만 추출
+            String jwt = jwtUtil.substringToken(authorizationHeader);
+
+            try {
+
+                /*
+                Claims - 페이로드 부분에 포함된 정보 -> 인증된 사용자에 대한 정보를 포함
+                이 claims 를 JWT 에서 추출함
+                 */
+                Claims claims = jwtUtil.extractClaims(jwt);
+                UserRole userRole = UserRole.of(claims.get("userRole", String.class)); // 사용자 권한 추출
+
+                /*
+                  SecurityContext - 사용자의 인증 정보를 저장하는 컨테이너 & 특정 작업에 대한 접근 제어 수행
+
+                  SecurityContextHolder - 이를 저장하고 관리하는 클래스
+                  -> 현재 스레드에 대한 SecurityContext 저장, 조회 가능
+
+                  SecurityContextHolder 가 비어있는지 판별하고 비어있다면 인증을 처리함
+                 */
+                if (SecurityContextHolder
+                        .getContext()
+                        .getAuthentication() == null) {
+
+                    AuthUser authUser = new AuthUser(userRole);
+
+                    // authUser 정보를 담은 토큰 생성 및 SecurityContext 에 인증 정보 저장
+                    JwtAuthenticationToken authenticationToken = new JwtAuthenticationToken(authUser);
+                    authenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(httpRequest));
+                    SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+                }
+            } catch (SecurityException | MalformedJwtException e) {
+                log.error("Invalid JWT signature, 유효하지 않는 JWT 서명 입니다.", e);
+                httpResponse.sendError(HttpServletResponse.SC_UNAUTHORIZED, "유효하지 않는 JWT 서명입니다.");
+            } catch (ExpiredJwtException e) {
+                log.error("Expired JWT token, 만료된 JWT token 입니다.", e);
+                httpResponse.sendError(HttpServletResponse.SC_UNAUTHORIZED, "만료된 JWT 토큰입니다.");
+            } catch (UnsupportedJwtException e) {
+                log.error("Unsupported JWT token, 지원되지 않는 JWT 토큰 입니다.", e);
+                httpResponse.sendError(HttpServletResponse.SC_BAD_REQUEST, "지원되지 않는 JWT 토큰입니다.");
+            } catch (Exception e) {
+                log.error("Internal server error", e);
+                httpResponse.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+            }
+        }
+        chain.doFilter(httpRequest, httpResponse);
+    }
+
+}
---
+++ src/main/java/com/example/onboarding/config/JwtUtil.java
@@ -0,0 +1,106 @@
+package com.example.onboarding.config;
+
+
+import com.example.onboarding.common.exception.CustomException;
+import com.example.onboarding.domain.user.enums.UserRole;
+import com.example.onboarding.common.enums.ErrorCode;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+
+import java.security.Key;
+import java.util.Base64;
+import java.util.Date;
+
+@Component
+public class JwtUtil {
+
+    /*
+    JWT( JSON Web Token )를 생성, 파싱 및 검증하는 기능을 제공하는 유틸리티
+    JWT 를 안전하게 생성하고, 이를 사용하여 클라이언트와 서버 간의 인증을 처리
+     */
+
+    private static final String BEARER_PREFIX = "Bearer "; // 접두사 - Bearer ~
+    private static final long TOKEN_TIME = 60 * 60 * 1000L; // 토큰 유효시간 - 60분
+
+    @Value("${SECRET_KEY}")
+    private String secretKey;
+    private Key key;
+    private final SignatureAlgorithm signatureAlgorithm = SignatureAlgorithm.HS256;
+
+    /**
+     * 클래스 초기화 메서드
+     * secretKey 를 Base64로 디코딩하여 HMAC 서명에 사용할 키를 초기화
+     * JWT 생성 및 검증을 위한 키 설정을 수행
+     */
+    @PostConstruct // PostConstruct 에너테이션 -
+    public void init() {
+        byte[] bytes = Base64.getDecoder().decode(secretKey);
+        key = Keys.hmacShaKeyFor(bytes);
+    }
+
+    /**
+     * JWT 를 생성하는 메서드
+     * 주어진 사용자 정보를 기반으로 JWT 를 생성
+     * 토큰은 사용자의 ID를 주체로 설정하고, 이메일 및 역할을 클레임으로 추가
+     * 토큰의 유효 기간 ( 60분 ), 발급일 포함
+     *
+     * @param userId   사용자 ID
+     * @param email    사용자 이메일
+     * @param userRole 사용자 권한 ( ROLE_USER, ROLE_ADMIN )
+     * @return 생성된 JWT 문자열 (Bearer 접두사 포함)
+     */
+    public String createToken(Long userId, String email, UserRole userRole) {
+        Date date = new Date();
+
+        return BEARER_PREFIX +
+                Jwts.builder()
+                        .setSubject(String.valueOf(userId))
+                        .claim("email", email)
+                        .claim("userRole", userRole)
+                        .setExpiration(new Date(date.getTime() + TOKEN_TIME))
+                        .setIssuedAt(date) // 발급일
+                        .signWith(key, signatureAlgorithm) // 암호화 알고리즘
+                        .compact();
+    }
+
+    /**
+     * Authorization 헤더에서 JWT 를 추출하는 메서드
+     * Authorization 헤더에서 Bearer 접두사를 제거하고 JWT 를 반환
+     * Bearer 접두사가 없거나 값이 비어있으면 CustomException 이 발생
+     *
+     * @param tokenValue Authorization 헤더의 값
+     * @return 추출된 JWT 문자열
+     * @throws CustomException TOKEN_NOT_FOUND 예외
+     */
+    public String substringToken(String tokenValue) {
+        if (StringUtils.hasText(tokenValue) && tokenValue.startsWith(BEARER_PREFIX)) {
+            return tokenValue.substring(7);
+        }
+        throw new CustomException(ErrorCode.TOKEN_NOT_FOUND);
+    }
+
+    /**
+     * 주어진 JWT 에서 클레임을 추출하는 메서드
+     * 입력된 JWT 를 파싱하고 서명 검증을 수행하여 유효성을 확인
+     * 유효한 JWT 일 경우 해당 JWT 에 포함된 클레임 정보를 반환
+     *
+     * @param token JWT 문자열 -> Bearer 접두사를 제외한 순수 JWT 값
+     * @return JWT 의 클레임 정보를 담고 있는 {@link Claims} 객체
+     * @throws io.jsonwebtoken.JwtException JWT 파싱 또는 서명 검증 실패 시 발생하는 예외
+     */
+    public Claims extractClaims(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(key)
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+}
